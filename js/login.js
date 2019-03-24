74126c8028278cfa9b1151515a30679e91dafc59
Tim Hegenkötter <t.hegenkoetter@web.de>
2019-03-24 11:04:24 +0100

    Formulardaten übertragen

--- js/login.js
+++ js/login.js
@@ -1,4 +1,12 @@
-document.getElementById("login-button").addEventListener("click",() => {
+console.log("Test")
+
+document.getElementById("login-button").addEventListener("click",
+() => {
+    
+    let email = document.getElementById("inputEmail").value;
+    let password = document.getElementById("inputPassword").value;
+    console.log(email);
+    console.log(password);
     loadingNav();
     corsRequest("https://almond-dunlin-3705.twil.io/sync-token", reqListener);
 });
